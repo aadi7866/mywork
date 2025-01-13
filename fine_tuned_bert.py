e66a3bc66044333c74a7600f79322df8ec6b2456
Asad Randhawa <62517481+aadi7866@users.noreply.github.com>
2025-01-13 06:59:03 -0800

    Create fine_tuned_bert.py

---
+++ fine_tuned_bert.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="osei1819/phishing_detection_fine_tuned_bert",truncation=True, max_length=256)
+
+
+input=st.text_area("write the testing text")
+
+result=pipe(input)
+st.write(result)
